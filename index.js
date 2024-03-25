5d0bf0c52daf959f4f828bda3b0f1ef895812ea0
George Castillo <gc21132@gmail.com>
2024-03-25 15:25:43 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const messages = [];
+    
+    for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message); 
+    }
+    
+    return messages; 
+  }
+  
+  const thankYouMessages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(thankYouMessages);
+function countDown(number) {
+    if (number <= 0) {
+        console.log("Please provide a positive integer greater than zero.");
+        return;
+    }
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+countDown(10);
